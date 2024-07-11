bb60d682128706d820af6144730c7055c272b486
KabirDCheema <93631807+KabirDCheema@users.noreply.github.com>
2024-07-11 06:02:56 +0200

    first small bits of code

---
+++ Orbit_prop_2/simulation.py
@@ -0,0 +1,11 @@
+from Utils import satellite
+
+def main():
+    run_sim()
+    
+def run_sim():
+    sat = satellite.Satellite()
+    print('run sim')
+
+if __name__ == '__main__':
+    main()
---
+++ Utils/satellite.py
@@ -0,0 +1,5 @@
+class Satellite:
+    def __init__(self, name : str | None = None):
+        if name is not None:
+            self.name = name
+    
